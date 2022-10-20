da2ec16f4fe6b5e8c29a0d29d3a38826f1ab4239
Priscilla <edpris.montalvo@gmail.com>
2022-10-19 21:56:16 -0500

    test

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 // import all models
-const Post = require('./Post');
+
 const User = require('./User');
 const Vote = require('./Vote');
 const Comment = require('./Comment');
+const Post = require('./Post');
 
 // create associations
 User.hasMany(Post, {
